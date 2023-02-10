e7d10e6eda6e26a7960e238ea0ff54a09040b934
Triyasa Das <91455154+triyasa-2021@users.noreply.github.com>
2023-02-10 18:40:13 +0530

    Add files via upload
    Initial commit

---
+++ cal.js
@@ -0,0 +1,11 @@
+function clr(){
+    document.getElementById("result").value = '';
+}
+function display(value){
+    document.getElementById("result").value += value;
+}
+function calculate(){
+    var x = document.getElementById("result").value;
+    var y = eval(x);
+    document.getElementById("result").value = y;
+}
